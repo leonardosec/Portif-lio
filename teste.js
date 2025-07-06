0b8c15336bf0e3f9f71865f903174be58d86804c
kernelx86xd <leozin247xd@gmail.com>
2025-07-06 01:45:55 -0300

    1commit

---
+++ teste.js
@@ -0,0 +1,140 @@
+const translations = {
+  pt: {
+    "menu.home": "Início",
+    "menu.about": "Sobre",
+    "menu.skills": "Habilidades",
+    "menu.experience": "Experiência",
+    "menu.projects": "Projetos",
+    "menu.contact": "Contato",
+    "menu.descri": "Red Teamer em formação, simulando ataques reais para descobrir falhas, antecipar ameaças e fortalecer defesas com inteligência ofensiva.",
+    "menu.Contact-me": "Contate-me",
+    "menu.my": "Meus Projetos",
+    "menu.meme": "Sobre Mim",
+    "menu.mydescri": "Tenho me dedicado intensamente à área de Red Team no último ano, desenvolvendo habilidades que vão desde testes de penetração até a elaboração e aplicação de políticas de segurança robustas.",
+    "menu.ofen": "Segurança Ofensiva",
+    "menu.teste": "Testes de penetração, análise de vulnerabilidades e red teaming.",
+    "menu.def": "Segurança Defensiva",
+    "menu.apli": "Segurança em Aplicações",
+    "menu.ana": "Análise de código seguro e testes de segurança em aplicações web.",
+    "menu.nu": "Segurança em Nuvem",
+    "menu.prote": "Proteção de ambientes cloud e arquiteturas seguras.",
+    "menu.ej": "Em busca da eJPT",
+    "menu.min": "Minhas Habilidades",
+    "menu.moni": "Monitoramento de segurança, resposta a incidentes e blue teaming.",
+    "menu.pen": "Testes de Penetração",
+    "menu.vul": "Análise de Vulnerabilidades",
+    "menu.seg": "Segurança em Redes",
+    "menu.fer": "Ferramentas & Tecnologias",
+    "menu.tec": "Habilidades Técnicas",
+    "menu.out": "Outras ferramentas:",
+    "menu.minha": "Minha Jornada",
+    "menu.aw": "Execução de testes em servidores e aplicações, Análise de vulnerabilidades e elaboração de relatórios técnicos, Configuração de firewalls (Check Point, Cisco), VPNs, IPS, e sistemas de controle de acesso, Participação ativa em projetos de segurança da informação.",
+    "menu.rem": "2024 - Remoto",
+    "menu.est": "Estagiário - T.I",
+    "menu.sup": "Suporte técnico e administração de redes, Configuração de equipamentos e serviços, Participação na resposta a incidentes de segurança internos.",
+    "menu.mp": "Meus Projetos",
+    "menu.mass": "Ferramenta de verificação massiva de Vulnerabilidades",
+    "menu.road": "Um roteiro estruturado e brutal que espelha como hackers russos de elite desenvolvem suas habilidades. Foca na construção sólida desde a base matemática até o domínio avançado em ofensiva, engenharia reversa, operações clandestinas e guerra cibernética.",
+    "menu.pro": "Futuros Projetos",
+    "menu.bru": "Brute force de login com tratamento de resposta - Ferramenta completa de Recon - C2 com shell reversa criptografada - Exploit local com shell injectado - Loader com evasão AV/EDR em C puro",
+    "menu.entre": "Entre em Contato",
+    "menu.na": "Nome",
+    "menu.su": "Assunto",
+    "menu.messa": "Mensagem",
+    "menu.enviar": "Enviar Mensagem",
+    "menu.1": "Início",
+    "menu.2": "Sobre",
+    "menu.3": "Habilidades",
+    "menu.4": "Experiência",
+    "menu.5": "Projetos",
+    "menu.6": "Contato",
+    "menu.redes": "Minhas Redes Socias",
+    "menu.siga": "Me siga nas redes e acompanhe meu trabalho e estudos em cibersegurança.",
+    "menu.2025": "2025 Leonardo Costa. Todos os direitos reservados.",
+    "menu.breve": "Em breve...",
+  },
+  en: {
+    "menu.home": "Home",
+    "menu.about": "About",
+    "menu.skills": "Skills",
+    "menu.experience": "Experience",
+    "menu.projects": "Projects",  
+    "menu.contact": "Contact",
+    "menu.descri": "Red Teamer in training, simulating real attacks to discover flaws, anticipate threats and strengthen defenses with advanced intelligence.",
+    "menu.Contact-me": "Contact-me",
+    "menu.my": "My Projects",
+    "menu.meme": "About me",
+    "menu.mydescri": "I have been intensely dedicated to the Red Team area over the last year, developing skills ranging from penetration testing to the development and application of robust security policies.",
+    "menu.ofen": "Offensive Security",
+    "menu.teste": "Penetration testing, vulnerability analysis and red teaming.",
+    "menu.def": "Defensive Security",
+    "menu.apli": "Application Security",
+    "menu.ana": "Secure code analysis and security testing of web applications.",
+    "menu.nu": "Cloud Security",
+    "menu.prote": "Protection of cloud environments and secure architectures.",
+    "menu.ej": "In search of eJPT",
+    "menu.min": "My Skills",
+    "menu.moni": "Security monitoring, incident response and blue teaming.",
+    "menu.pen": "Penetration Tests",
+    "menu.vul": "Vulnerability Analysis",
+    "menu.seg": "Network Security",
+    "menu.fer": "Tools and Technologies",
+    "menu.tec": "Technical Skills",
+    "menu.out": "Other Tools:",
+    "menu.minha": "My Journey",
+    "menu.aw": "Execution of tests on servers and applications, Analysis of vulnerabilities and preparation of technical reports, Configuration of firewalls (Check Point, Cisco), VPNs, IPS, and access control systems, Active participation in information security projects.",
+    "menu.rem": "2024 - Remote",
+    "menu.est": "Intern - IT",
+    "menu.sup": "Technical support and network administration, Configuration of equipment and services, Participation in the response to internal security incidents.",
+    "menu.mp": "My Projects",
+    "menu.mass": "Massive Vulnerability Scanning Tool",
+    "menu.road": "A structured and brutal roadmap that mirrors how elite Russian hackers develop their skills. It focuses on building solid foundations from mathematical knowledge to advanced mastery in offensive, reverse engineering, clandestine operations, and cyber warfare.",
+    "menu.pro": "Future projects",
+    "menu.bru": "Brute force login with response handling - Complete Recon tool - C2 with encrypted reverse shell - Local exploit with injected shell - Loader with AV/EDR evasion in pure C",
+    "menu.entre": "Get in touch",
+    "menu.na": "Name",
+    "menu.su": "Subject",
+    "menu.messa": "Message",
+    "menu.enviar": "Send Message",
+    "menu.1": "Home",
+    "menu.2": "About",
+    "menu.3": "Skills",
+    "menu.4": "Experience",
+    "menu.5": "Projects",
+    "menu.6": "Contact",
+    "menu.redes": "My social networks",
+    "menu.siga": "Follow me on social media and keep up with my work and studies in cybersecurity.",
+    "menu.2025": "2025 Leonardo Costa. All rights reserved.",
+    "menu.breve": "shortly...",
+  }
+};
+
+const languageSwitcher = document.getElementById("languageSwitcher");
+
+function updateLanguage(lang) {
+  document.querySelectorAll("[data-i18n]").forEach(el => {
+    const key = el.getAttribute("data-i18n");
+    if (translations[lang][key]) {
+      el.textContent = translations[lang][key];
+    }
+  });
+}
+
+languageSwitcher.addEventListener("change", (e) => {
+  updateLanguage(e.target.value);
+});
+
+// Define idioma padrão ao carregar
+updateLanguage("pt");
+
+// Função para remover o cursor após a animação terminar
+function removeCursor() {
+    const typewriterElement = document.querySelector('.typewriter');
+    if (typewriterElement) {
+        // Remove o cursor piscante
+        typewriterElement.style.borderRight = 'none';
+    }
+}
+
+// Aguarde 3.5 segundos (tempo da animação) e remova o cursor
+setTimeout(removeCursor, 3500);
