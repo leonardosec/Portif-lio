0ca00d1f1091793eb0eb20b3f9ff61a7446b545e
kernelx86xd <leozin247xd@gmail.com>
2025-07-09 01:48:54 -0300

    mudança no icone do card do troja

--- teste.js
+++ teste.js
@@ -51,7 +51,7 @@ const translations = {
     "menu.redes": "Minhas Redes Socias",
     "menu.siga": "Me siga nas redes e acompanhe meu trabalho e estudos em cibersegurança.",
     "menu.2025": "2025 Leonardo Costa. Todos os direitos reservados.",
-    "menu.tro": "Trojan Modular para Estudos de Red Team",
+    "menu.tro": "Trojan Modular",
     "menu.horse": "Trojan modular com shell reverso, persistência via registro do Windows e payload criptografado com AES. Criado para simular técnicas ofensivas em ambientes controlados, com foco em evasão e execução furtiva.",
     "menu.furuto": "Futuros Projetos"
   },
@@ -107,7 +107,7 @@ const translations = {
     "menu.redes": "My social networks",
     "menu.siga": "Follow me on social media and keep up with my work and studies in cybersecurity.",
     "menu.2025": "2025 Leonardo Costa. All rights reserved.",
-    "menu.tro": "Modular Trojan for Red Team Studies",
+    "menu.tro": "Modular Trojan",
     "menu.horse": "Modular Trojan with reverse shell, Windows registry persistence and AES-encrypted payload. Designed to simulate offensive techniques in controlled environments, with a focus on evasion and stealth execution.",
     "menu.futuro": "Future projects"
   }
